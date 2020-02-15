65935c2444b2859af4ab85288b6fc9cdd26dc43a
鄭涵彧 <amandacheng@MBA.local>
2020-02-14 18:43:24 -0800

    add week 6 exercises

--- chapter-18-exercises/exercise-1/exercise.R
+++ chapter-18-exercises/exercise-1/exercise.R
@@ -1,16 +1,17 @@
 # load relevant libraries
 library("httr")
 library("jsonlite")
+library("dplyr")
 
 # Be sure and check the README.md for complete instructions!
 
 
 # Use `source()` to load your API key variable from the `apikey.R` file you made.
 # Make sure you've set your working directory!
-
+api_key <- source("apikey.R") # go to that file, select all, and hit c-enter
 
 # Create a variable `movie_name` that is the name of a movie of your choice.
-
+movie_name <- "Parasite"
 
 # Construct an HTTP request to search for reviews for the given movie.
 # The base URI is `https://api.nytimes.com/svc/movies/v2/`
@@ -20,26 +21,42 @@ library("jsonlite")
 #
 # You should use YOUR api key (as the `api-key` parameter)
 # and your `movie_name` variable as the search query!
+base_uri <- "https://api.nytimes.com/svc/movies/v2/"
+endpoint <- "reviews/search.json"
+resource_uri <- paste0(base_uri, endpoint)
 
+query_params <- list("api-key" = nyt_key, "movie_name" = movie_name)
 
 # Send the HTTP Request to download the data
 # Extract the content and convert it from JSON
-
+response <- GET(resource_uri, query = query_params)
+body <- content(response, "text", encoding = "UTF-8")
+data <- fromJSON(body)
 
 # What kind of data structure did this produce? A data frame? A list?
-
+class(data)
 
 # Manually inspect the returned data and identify the content of interest 
 # (which are the movie reviews).
 # Use functions such as `names()`, `str()`, etc.
-
+data$results
 
 # Flatten the movie reviews content into a data structure called `reviews`
-
+reviews <- flatten(data$results)
 
 # From the most recent review, store the headline, short summary, and link to
 # the full article, each in their own variables
-
+most_recent <- reviews %>% 
+  arrange(date_updated) %>% 
+  top_n(1, date_updated)
+headline <- most_recent %>% 
+  pull(headline)
+short_summary <- most_recent %>% 
+  pull(summary_short)
+link_to_article <- most_recent %>% 
+  pull(link.url)
 
 # Create a list of the three pieces of information from above. 
 # Print out the list.
+info <- list(headline = headline, short_summary = short_summary, link_to_article = link_to_article)
+print(info)
